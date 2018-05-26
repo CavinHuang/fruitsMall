d4a86426fe49edaa0d9990263ee3a3723fedd37e
CavinHuang <943866180@qq.com>
2018-05-26 14:05:27 +0800

    创建开发分支

--- config/database.php
+++ config/database.php
@@ -15,13 +15,13 @@
 return [
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '47.94.136.43',
     // 数据库名
     'database'        => 'fruitsmall',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'cavin',
     // 密码
-    'password'        => '10idccom@',
+    'password'        => 'zhcm1993',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
